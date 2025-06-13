9c03339276ba6998ac28b8689e2717511f186d48
renalyng17 <renagiray17@gmail.com>
2025-06-13 11:23:54 +0800

    font

---
+++ src/pages/Admin/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="p-6 text-xl font-semibold text-gray-800">
+      Home Dashboard
+    </div>
+  );
+};
+
+export default Home;
