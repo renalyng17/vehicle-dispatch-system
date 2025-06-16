36cfe34b48cfbb000c6a7f607bcce821ac3ba76d
renalyng17 <renagiray17@gmail.com>
2025-06-16 08:41:04 +0800

    head

--- src/pages/Admin/Management.jsx
+++ src/pages/Admin/Management.jsx
@@ -1,3 +1,3 @@
 export default function Management() {
-  return <h1 className="p-6 text-3xl font-bold">Management Page</h1>;
+  return <h1 className="alignment-top p-6 text-3xl font-bold">Management Page</h1>;
 }
